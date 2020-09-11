a93b02fd9093a411e1d031ddb11e36f3a411e2c0
TNT-df <1352465154@qq.com>
2020-09-11 17:47:16 +0800

    组合总数(3)

--- 组合总数(3).java
+++ 组合总数(3).java
@@ -1,4 +1,5 @@
 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+
 说明：
 
 所有数字都是正整数。
@@ -12,7 +13,6 @@
 输入: k = 3, n = 9
 输出: [[1,2,6], [1,3,5], [2,3,4]]
 
-。
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
