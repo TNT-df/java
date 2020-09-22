85f5462fa4950a58d5371bd38ee1d95c62893eae
TNT-df <135246514@qq.com>
2020-09-22 20:44:01 +0800

    删除排序链表中的重复元素

---
+++ 删除排序链表中的重复元素.java
@@ -0,0 +1,35 @@
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+    if(head == null){
+        return null;
+    }
+    ListNode node = head;
+    while(node!=null&&node.next!=null){
+        if(node.val==node.next.val){
+            node.next= node.next.next;
+        }
+        else{
+            node=node.next;
+        }
+    }
+    return head;}
+}
