c85840cd88c34ee9739c08b7bbe962eb5b3c5192
TNT-df <1352465154@qq.com>
2021-01-12 15:30:52 +0800

    数组

--- acwing/蛇形数组.java
+++ acwing/蛇形数组.java
@@ -79,3 +79,39 @@ public class Main {
         }
     }
 }
+//方法二
+
+import java.util.*;
+public class Main{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] q = new int[110][110];
+        int[] dx=  {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        int d = 1,x = 0 ,y = 0;
+        for(int i = 1;i<=n*m;i++){
+            q[x][y] = i;
+            int a = x + dx[d];
+            int b = y + dy[d];
+            //碰壁或者当前位置有数则进行翻转90°
+          if((a < 0 || a >= n || b < 0 || b >= m)||q[a][b]!=0){
+              d= (d + 1)%4;
+              a = x + dx[d];
+              b = y + dy[d];
+          }
+          x = a;
+          y = b;
+        }
+        for(int i = 0 ; i<n;i++){
+            for(int j = 0;j<m;j++){
+                System.out.print(q[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
